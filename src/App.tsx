e185bb6f49b42cde85b740012e306faa1382014e
Danil Illarjonov <90372410+Illarjonov@users.noreply.github.com>
2023-02-01 15:45:36 +0300

    Компонент поиска с задержкой + удалил лишний код

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,27 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import { API, Data } from './api/api';
 import { Airport } from './components/airport/airport';
+import { SearchInput } from './components/search-input/search-input';
 
 function App() {
   const [data, setData] = useState<Data[]>([])
 
-  useEffect(() => {
-    API.searchAirport() //fetch by default
+  const fetchByDefault = () => {
+    API.searchAirport()
       .then((res: any) => {
         if (res.status === 200) {
           setData(res.data)
         }
       })
-  }, [])
+  }
+
+  useEffect(fetchByDefault, [])
 
   return (
     <div className="App">
-      
+      <div>
+        <SearchInput/>
+      </div>
       <div>
         {data.map((item, index) => <Airport data={item} key={index} />)}
       </div>
---
+++ src/components/search-input/search-input.tsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react"
+import s from './search-input.module.css'
+
+export const SearchInput = () => {
+    const [searchText, setSearchText] = useState('')
+    
+    useEffect(() => {
+        const timeOutId = setTimeout(() => console.log(searchText), 700)
+        return () => clearTimeout(timeOutId)
+    }, [searchText])
+
+    return <>
+        <input
+            onChange={e => setSearchText(e.target.value)}
+            placeholder="Send text. Input use debounce 0.7 ms"
+            className={s.input} />
+    </>
+}  
